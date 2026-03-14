9bfe5d5a67091a142e38cd7a43bee6a1151627d4
Manuel Meza <mmezari@ucvvirtual.edu.pe>
2026-03-14 04:15:05 -0500

    .

--- frontend/src/pages/students/MyCoursesPage.tsx
+++ frontend/src/pages/students/MyCoursesPage.tsx
@@ -123,7 +123,7 @@ const handleSearch = () => {
               <h3 className="font-semibold text-gray-800">{course.titulo}</h3>
 
               <p
-                onClick={() => navigate(`/admin/courses/${course.id}/modules`)}
+                onClick={() => navigate(`/student/courses/${course.id}/modules`)}
                 className="text-xs text-gray-500 cursor-pointer"
               >
                 {course.modules_count} modulos
